901d35dfb6f320fca8b6d8546cc3da33cd10d3ba
Les Hazlewood <les@hazlewood.com>
2016-10-19 10:01:07 -0700

    cleaned up exclusion list so sphinx doesn't print warnings

--- docs/.scms.groovy
+++ docs/.scms.groovy
@@ -1,5 +1,7 @@
+def baseExcludes = ['build.sh', 'build/**', 'Makefile', 'readme.md']
+
 scms {
-    excludes = ['build.sh', 'build/**', 'Makefile', 'readme.md']
+    excludes = baseExcludes
 
     patterns {
 
@@ -13,16 +15,44 @@ scms {
 
 environments {
 
+    def springExcludes = baseExcludes + ['source/access-control.rst', 'source/appendix/web-stormpath-properties.rst']
+
     servlet {
-        scms.model.servlet = true
+        scms {
+            excludes = baseExcludes +
+                        //the following files are not relevant for the servlet docs.  We exclude them here
+                        //so the sphinx parser doesn't warn us during rendering, e.g.
+                        //    WARNING: document isn't included in any toctree
+                        ['source/appendix/default-stormpath-properties.rst',
+                        'source/appendix/spring-boot-core-properties.rst',
+                        'source/appendix/spring-boot-web-properties.rst',
+                        'source/appendix/forgot-password.rst',
+                        'source/appendix/change-password.rst',
+                        'source/appendix/head.rst',
+                        'source/appendix/login.rst',
+                        'source/appendix/register.rst',
+                        'source/appendix/verify.rst']
+            model {
+                servlet = true
+            }
+        }
     }
 
     sczuul {
-        scms.model.sczuul = true
+        scms {
+            excludes = springExcludes
+            model {
+                sczuul = true
+            }
+        }
     }
 
     springboot {
-        scms.model.springboot = true
+        scms {
+            excludes = springExcludes
+            model {
+                springboot = true
+            }
+        }
     }
-
 }
